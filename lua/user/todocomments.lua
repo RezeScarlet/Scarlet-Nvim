5475e4146e28dd2f9fb597cc27c7d0eea556210a
RezeScarlet <gui171204@gmail.com>
2024-02-15 14:13:04 -0300

    Fixed a problem with C++ language server

--- lua/user/todocomments.lua
+++ lua/user/todocomments.lua
@@ -6,17 +6,24 @@ local M = {
     sign_priority = 8, -- sign priority
     -- keywords recognized as todo comments
     keywords = {
+      -- FIX 
       FIX = {
         icon = " ", -- icon used for the sign, and in search results
         color = "error", -- can be a hex color, or a named color (see below)
         alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
         -- signs = false, -- configure signs for some keywords individually
       },
+      -- TODO 
       TODO = { icon = " ", color = "info" },
+      -- HACK 
       HACK = { icon = " ", color = "warning" },
+      -- WARN
       WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+      -- PERF
       PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+      -- NOTE 
       NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+      -- TEST 
       TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
     },
     gui_style = {
@@ -33,9 +40,9 @@ local M = {
       multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
       multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
       before = "", -- "fg" or "bg" or empty
-      keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+      keyword = "bg", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
       after = "fg", -- "fg" or "bg" or empty
-      pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
+      pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
       comments_only = true, -- uses treesitter to match keywords in comments only
       max_line_len = 400, -- ignore lines longer than this
       exclude = {}, -- list of file types to exclude highlighting
@@ -61,8 +68,8 @@ local M = {
       },
       -- regex that will be used to match keywords.
       -- don't replace the (KEYWORDS) placeholder
-      pattern = [[\b(KEYWORDS):]], -- ripgrep regex
-      -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+      -- pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+      pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
     },
   },
 }
