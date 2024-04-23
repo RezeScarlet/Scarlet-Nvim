2b36674a1a0abae8f4b632600785ae1a12e178c7
RezeScarlet <gui171204@gmail.com>
2024-04-23 16:13:49 -0300

    No text autocomplete and Ctrl-c to copy entire file

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -5,6 +5,7 @@ keymap("n", "<Space>", "", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+keymap("n", "<C-c>", ":%y+<CR>")
 keymap("n", "<C-i>", "<C-i>", opts)
 
 -- Better window navigation
